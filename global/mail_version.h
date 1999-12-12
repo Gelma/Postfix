b4ebc8228f6956b6229b54e5b21ac7c66824980b
Wietse Zweitze Venema <wietse@porcupine.org>
1999-12-12 02:26:00 +0100

    Postfix 0.8-pl09
    postfix-19990906-patch09.gz

--- global/mail_version.h
+++ global/mail_version.h
@@ -15,7 +15,7 @@
   * Version of this program.
   */
 #define VAR_MAIL_VERSION	"mail_version"
-#define DEF_MAIL_VERSION	"Postfix-19990906-pl08"
+#define DEF_MAIL_VERSION	"Postfix-19990906-pl09"
 extern char *var_mail_version;
 
 /* LICENSE
