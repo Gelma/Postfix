f14c7b78dc4ef1c0a7347c97844e827e5a136c12
Wietse Zweitze Venema <wietse@porcupine.org>
2000-11-22 16:48:00 +0100

    Postfix 0.9-pl11
    postfix-19991231-patch11.gz

--- global/mail_version.h
+++ global/mail_version.h
@@ -15,7 +15,7 @@
   * Version of this program.
   */
 #define VAR_MAIL_VERSION	"mail_version"
-#define DEF_MAIL_VERSION	"Postfix-19991231-pl10"
+#define DEF_MAIL_VERSION	"Postfix-19991231-pl11"
 extern char *var_mail_version;
 
 /* LICENSE
